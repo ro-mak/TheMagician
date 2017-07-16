b2109e491ff17c68784583abae998a2155a2eb50
ro-mak <eee127@yandex.ru>
2017-07-16 15:48:16 +0300

    Added Circle field to abstract class GameObject and a getter

--- core/src/com/makproductions/magician/GameObject.java
+++ core/src/com/makproductions/magician/GameObject.java
@@ -2,15 +2,19 @@ package com.makproductions.magician;
 
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Circle;
 import com.badlogic.gdx.math.Vector2;
 
 public abstract class GameObject {
 
     Vector2 position;
     Texture texture;
-
+    Circle circle;
     public abstract void render(SpriteBatch batch);
     public abstract void update();
+    public Circle getCircle(){
+        return circle;
+    }
 
     public Vector2 getPosition() {
         return position;
