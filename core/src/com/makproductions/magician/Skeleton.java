cef914b7669de2be617ff5b2fe276b9e687c600d
ro-mak <eee127@yandex.ru>
2017-07-16 16:01:41 +0300

    1) extended GameObject 2) added health field 3) added isAlive getter 4) added health setter 5) moved animation from constructor to render method 6) refactored method fire -> attack 7) added checks for hero life status 8) added skeleton attack mode control by assigning: attacking = false, if hero is dead or out of range.

--- core/src/com/makproductions/magician/Skeleton.java
+++ core/src/com/makproductions/magician/Skeleton.java
@@ -8,14 +8,29 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Circle;
 import com.badlogic.gdx.math.Vector2;
 
-public class Skeleton implements Unit {
+public class Skeleton extends GameObject implements Unit {
 
     private static final int FRAME_COLS = 2, FRAME_ROWS = 1;
 
     private Texture texture;
+    private int health = 100;
     Animation<TextureRegion> walkAnimation;
     TextureRegion[][] tmp;
     TextureRegion[] walkFrames;
+
+    public boolean isAlive() {
+        if(health <= 0){
+            return false;
+        }
+        return alive;
+    }
+
+    @Override
+    public void setHealth(int health) {
+        this.health -= health;
+        System.out.println(this + " got hit:" + health);
+        System.out.println(this + "Remaining health: "+this.health);
+    }
 
     @Override
     public Circle getCircle() {
@@ -29,9 +44,7 @@ public class Skeleton implements Unit {
     private boolean alive = false;
     private boolean attacking = false;
     private float speed;
-    public boolean isAlive() {
-        return alive;
-    }
+
     public boolean isAttacking() {
         return attacking;
     }
@@ -39,53 +52,59 @@ public class Skeleton implements Unit {
     public Skeleton(){
         this.speed = 1;
         this.alive = true;
-        this.position = new Vector2(1000,300);
+        this.position = new Vector2(1000,(float) (Math.random()*300));
         this.texture = new Texture("SkeletonSpriteSheet.png");
-        this.circle = new Circle(position,350);
-        tmp = TextureRegion.split(texture,
-                texture.getWidth() / FRAME_COLS,
-                texture.getHeight() / FRAME_ROWS);
+        this.circle = new Circle(position,100);
 
-        walkFrames = new TextureRegion[FRAME_COLS * FRAME_ROWS];
-        int index = 0;
-        for (int i = 0; i < FRAME_ROWS; i++) {
-            for (int j = 0; j < FRAME_COLS; j++) {
-                walkFrames[index++] = tmp[i][j];
-            }
-        }
-        walkAnimation = new Animation<TextureRegion>(0.5f, walkFrames);
-        stateTime = 0f;
     }
 
     public void render(SpriteBatch batch){
         if(isAlive()) {
+
+            tmp = TextureRegion.split(texture,
+                    texture.getWidth() / FRAME_COLS,
+                    texture.getHeight() / FRAME_ROWS);
+
+            walkFrames = new TextureRegion[FRAME_COLS * FRAME_ROWS];
+            int index = 0;
+            for (int i = 0; i < FRAME_ROWS; i++) {
+                for (int j = 0; j < FRAME_COLS; j++) {
+                    walkFrames[index++] = tmp[i][j];
+                }
+            }
+            walkAnimation = new Animation<TextureRegion>(0.5f, walkFrames);
+
             stateTime += Gdx.graphics.getDeltaTime();
-            currentFrame = walkAnimation.getKeyFrame(stateTime, true);
+
+                currentFrame = walkAnimation.getKeyFrame(stateTime, true);
+
             batch.draw(currentFrame, position.x, position.y);
         }
     }
 
-    public void fire(Unit unit){
-        Vector2 unitPosition = unit.getPosition();
-        Circle attackCircle = new Circle(position,400);
-        if(this.circle.contains(unit.getPosition())){
-            attacking = false;
-            alive = false;
 
-        }else if(attackCircle.contains(unitPosition)){
-            attacking = true;
-            if (unit.getPosition().x > position.x && position.x < 1280 && position.x > 0) {
-                position.x+=speed;
-            } else if (unit.getPosition().x < position.x && position.x > 0 && position.x < 1280) {
-                position.x-=speed;
+
+    public void attack(Unit unit) {
+            Vector2 unitPosition = unit.getPosition();
+            Circle attackCircle = new Circle(position, 400);
+
+            if (unit.isAlive() && attackCircle.contains(unitPosition)) {
+                attacking = true;
+                if (unit.getPosition().x > position.x && position.x < 1280 && position.x > 0) {
+                    position.x += speed;
+                } else if (unit.getPosition().x < position.x && position.x > 0 && position.x < 1280) {
+                    position.x -= speed;
+                }
+
+                if (unit.getPosition().y > position.y && position.y > 0 && position.y < 768) {
+                    position.y += speed;
+                } else if (unit.getPosition().y < position.y && position.y > 0 && position.y < 768) {
+                    position.y -= speed;
+                }
+            }else{
+                attacking = false;
             }
 
-            if (unit.getPosition().y  > position.y && position.y > 0 && position.y < 768) {
-                position.y+=speed;
-            } else if (unit.getPosition().y < position.y && position.y > 0 && position.y < 768) {
-                position.y-=speed;
-            }
-        }
     }
 
     @Override
@@ -95,11 +114,14 @@ public class Skeleton implements Unit {
 
 
     public void move(){
-        if(!isAttacking())position.x -= speed;
+        if(!attacking)position.x -= speed;
     }
 
     public void update(){
-        move();
+        if(isAlive()) {
+            move();
+            circle.setPosition(position);
+        }
     }
     public void dispose(){
         texture.dispose();
