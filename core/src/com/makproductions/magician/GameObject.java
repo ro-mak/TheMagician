142b262928e6e7725074ab6bfd543d49a3b7142e
ro-mak <eee127@yandex.ru>
2017-07-16 20:05:28 +0300

    added method checkBounds to make objects stay on the screen

--- core/src/com/makproductions/magician/GameObject.java
+++ core/src/com/makproductions/magician/GameObject.java
@@ -23,4 +23,17 @@ public abstract class GameObject {
     public void setPosition(Vector2 position) {
         this.position = position;
     }
+     void checkBounds(){
+
+        if(position.x > 1280){
+            position.x = 1280;
+        }else if(position.x < 0){
+            position.x = 0;
+        }
+        if(position.y > 400){
+            position.y = 400;
+        }else if(position.y < 0){
+            position.y = 0;
+        }
+    }
 }
