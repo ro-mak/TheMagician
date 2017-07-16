c5a84b2ce16e5ebde19ef245aed9c0e911cbc64f
ro-mak <eee127@yandex.ru>
2017-07-16 20:08:05 +0300

    1) Now it's an abstract class extending GameObject so that there were no redundant methods.

--- core/src/com/makproductions/magician/Unit.java
+++ core/src/com/makproductions/magician/Unit.java
@@ -1,12 +1,10 @@
 package com.makproductions.magician;
 
-import com.badlogic.gdx.math.Circle;
-import com.badlogic.gdx.math.Vector2;
 
-public interface Unit {
-    Vector2 getPosition();
-    void setPosition(Vector2 position);
-    Circle getCircle();
-    void setHealth(int health);
-    boolean isAlive();
+
+
+public abstract class Unit extends GameObject {
+
+    abstract void setHealth(int health);
+    abstract boolean isAlive();
 }
